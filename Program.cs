3bf61500c87ded9712081a7f2e0661ad4f4b6174
osamaline1229 <osama98138139@gmail.com>
2023-07-11 01:40:12 -0700

    update

--- Program.cs
+++ Program.cs
@@ -8,13 +8,24 @@ namespace Stack
         private static void Main(string[] args)
         {
             
-            stack mystack = new stack();
-            mystack.
-          
+            stack<int> mystack = new stack<int>();
+            mystack.push(100);
+            mystack.push(200);
+            mystack.push(300);
+            mystack.push(400);
+            mystack.push(500);
+            mystack.pop();
+            Console.WriteLine(mystack.pop());
+            mystack.peek();
+            Console.WriteLine(mystack.peek());
 
 
 
-            
+
+
+
+
+
 
         }
         
