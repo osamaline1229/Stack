73c0e0dbe04874f4071b1a001968b86004c51006
osamaline1229 <osama98138139@gmail.com>
2023-07-11 01:21:42 -0700

    Stack Implementation

--- Program.cs
+++ Program.cs
@@ -8,39 +8,16 @@ namespace Stack
         private static void Main(string[] args)
         {
             
-            stack<int> mystack = new stack<int>();
-            mystack.push(100);
-            mystack.push(200);
-            mystack.push(300);
-            mystack.push(400);
-            mystack.push(600);
+            stack mystack = new stack();
+            mystack.
+          
 
 
-            Console.WriteLine("The Stack vlaue are :");
-            printStackvalue(mystack);
-            Console.WriteLine("The Lenght of My stack is:");
-            Console.WriteLine(mystack.count);
-            Console.WriteLine();
-            mystack.Pop();
-            mystack.Peek();
-            Console.WriteLine("The Stack vlaue are :" + mystack.Peek());
-            printStackvalue(mystack);
 
-            Console.WriteLine("The Stack vlaue are :");
-            mystack.Push("the top of Stack");
-            printStackvalue(mystack);
+            
 
         }
-        public static void printStackvalue(IEnumerable myCollection)
-        {
-            foreach (var i in myCollection)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine();
-        }
-
+        
     }
 
 
---
+++ Stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        private int[] stack;
+        private int top;
+
+        public Stack(int n)
+        {
+            this.stack = new int[n];
+            top = -1;
+        }
+
+
+
+
+
+
+        //public static void printStackvalue(IEnumerable myCollection)
+        //{
+        //    foreach (var i in myCollection)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //    Console.WriteLine();
+        //}
+        public void push(int number)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is full");
+                return;
+            }
+            //top++
+            stack[++top] = number;
+
+        }
+
+        public int peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            return stack[top];
+        }
+
+        private bool IsEmpty()
+        {
+            return top == -1;
+        }
+        private bool IsFull()
+        {
+            return top >= stack.Length - 1;
+        }
+    }
+    
+
+}
