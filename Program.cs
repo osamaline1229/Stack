8573391ea01739f10feb634270ab942e762f0a34
osamaline1229 <osama98138139@gmail.com>
2023-07-11 00:51:21 -0700

    stack

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            stack mystack = new stack();
+            mystack.push(100);
+
+
+            Console.WriteLine("The Stack vlaue are :");
+            printStackvalue(st);
+            Console.WriteLine("The Lenght of My stack is:");
+            Console.WriteLine(mystack.Count);
+            Console.WriteLine();
+            mystack.Pop();
+            mystack.Peek();
+            Console.WriteLine("The Stack vlaue are :" + mystack.Peek());
+            printStackvalue(mystack);
+
+            Console.WriteLine("The Stack vlaue are :");
+            mystack.Push("the top of Stack");
+            printStackvalue(mystack);
+
+        }
+
+
+    }
+
+
+}
---
+++ Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class stack
+    {
+        
+        
+        
+        
+
+
+        public static void printStackvalue(IEnumerable myCollection)
+        {
+            foreach (var i in myCollection)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
